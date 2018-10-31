2c6370e802ba4b5d750cb4187eac000ea256d5bc
ylx <191511840@qq.com>
2018-10-31 18:36:43 +0800

    初始化Springcloud

---
+++ src/main/java/com/wz/SpringcloudDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudDemoApplication.class, args);
+    }
+}
